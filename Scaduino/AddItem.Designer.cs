3b689838fad8a34bbe42cdd801299be341bfbe41
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-07-07 09:49:03 -0300

    feat: created AddItem dialog

---
+++ Scaduino/AddItem.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace Scaduino
+{
+    partial class AddItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Data Aquisition", 1, 1);
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Data Server", 2, 2);
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Devices", 3, 3);
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("File", 4, 4);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddItem));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.images = new System.Windows.Forms.ImageList(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.listView = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btAdd);
+            this.splitContainer1.Panel2.Controls.Add(this.btCancel);
+            this.splitContainer1.Panel2.Controls.Add(this.tbName);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(745, 522);
+            this.splitContainer1.SplitterDistance = 456;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(745, 456);
+            this.splitContainer2.SplitterDistance = 213;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.ImageIndex = 0;
+            this.treeView.ImageList = this.images;
+            this.treeView.Location = new System.Drawing.Point(0, 18);
+            this.treeView.Name = "treeView";
+            treeNode1.ImageIndex = 1;
+            treeNode1.Name = "Nó0";
+            treeNode1.SelectedImageIndex = 1;
+            treeNode1.Text = "Data Aquisition";
+            treeNode2.ImageIndex = 2;
+            treeNode2.Name = "Nó1";
+            treeNode2.SelectedImageIndex = 2;
+            treeNode2.Text = "Data Server";
+            treeNode3.ImageIndex = 3;
+            treeNode3.Name = "Nó2";
+            treeNode3.SelectedImageIndex = 3;
+            treeNode3.Text = "Devices";
+            treeNode4.ImageIndex = 4;
+            treeNode4.Name = "Nó3";
+            treeNode4.SelectedImageIndex = 4;
+            treeNode4.Text = "File";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+            this.treeView.SelectedImageIndex = 0;
+            this.treeView.Size = new System.Drawing.Size(213, 438);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // images
+            // 
+            this.images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("images.ImageStream")));
+            this.images.TransparentColor = System.Drawing.Color.Transparent;
+            this.images.Images.SetKeyName(0, "dataLogger.ico");
+            this.images.Images.SetKeyName(1, "dataAquisition.ico");
+            this.images.Images.SetKeyName(2, "dataServer.ico");
+            this.images.Images.SetKeyName(3, "devices.ico");
+            this.images.Images.SetKeyName(4, "application.ico");
+            this.images.Images.SetKeyName(5, "screens.ico");
+            this.images.Images.SetKeyName(6, "styles.ico");
+            this.images.Images.SetKeyName(7, "scripts.ico");
+            this.images.Images.SetKeyName(8, "data.ico");
+            this.images.Images.SetKeyName(9, "opcDa.png");
+            this.images.Images.SetKeyName(10, "serialJson.png");
+            this.images.Images.SetKeyName(11, "http.ico");
+            this.images.Images.SetKeyName(12, "serial.png");
+            this.images.Images.SetKeyName(13, "usbWifiHmiServer.ico");
+            this.images.Images.SetKeyName(14, "hmiServerStation.ico");
+            this.images.Images.SetKeyName(15, "html.png");
+            this.images.Images.SetKeyName(16, "css.png");
+            this.images.Images.SetKeyName(17, "js.png");
+            this.images.Images.SetKeyName(18, "json.png");
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Item Type";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.listView);
+            this.splitContainer3.Panel1.Controls.Add(this.label3);
+            this.splitContainer3.Size = new System.Drawing.Size(528, 456);
+            this.splitContainer3.SplitterDistance = 291;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // listView
+            // 
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.HideSelection = false;
+            this.listView.LargeImageList = this.images;
+            this.listView.Location = new System.Drawing.Point(0, 18);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(291, 438);
+            this.listView.SmallImageList = this.images;
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.List;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Item to Add";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(580, 29);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(661, 29);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 23);
+            this.btCancel.TabIndex = 2;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(55, 8);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(462, 20);
+            this.tbName.TabIndex = 1;
+            this.tbName.TextChanged += new System.EventHandler(this.tbName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // AddItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 522);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "AddItem";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Item";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ImageList images;
+    }
+}
---
+++ Scaduino/AddItem.cs
@@ -0,0 +1,172 @@
+﻿using DotNetCom.OpcDa;
+using DotNetCom.SerialInterface;
+using DotNetCom.Text;
+using DotNetCom.Web;
+using ScaduinoDevices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Scaduino
+{
+    public partial class AddItem : Form
+    {
+        public enum RootNode : int
+        {
+            DataAquisition,
+            DataServer,
+            Devices,
+            Files
+        }
+
+        public dynamic ItemToAdd { get; set; }
+
+        public string ItemName { get => tbName.Text; }
+
+        public static string Directory { get; set; }
+
+        private ListViewItem[] dataAquisitionItems =
+        {
+            new ListViewItem()
+            {
+                Text = "OPC DA", 
+                Name = "OPC DA",
+                ImageIndex = (int)AppTree.Image.OpcDa,
+                Tag = new OpcClient()
+            },
+            new ListViewItem()
+            {
+                Text = "Serial Json",
+                Name = "Serial Json",
+                ImageIndex = (int)AppTree.Image.SerialJson,
+                Tag = new SerialText()
+            },
+        };
+
+        private ListViewItem[] dataServerItems =
+        {
+            new ListViewItem()
+            {
+                Text = "Http Server",
+                Name = "Http Server",
+                ImageIndex = (int)AppTree.Image.Http,
+                Tag = new HttpTagsServer()
+            },
+            new ListViewItem()
+            {
+                Text = "Serial",
+                Name = "Serial",
+                ImageIndex = (int)AppTree.Image.Serial,
+                Tag = new SerialTagsServer()
+            },
+        };
+
+        private ListViewItem[] devicesItems =
+        {
+            new ListViewItem()
+            {
+                Text = "USB WiFi HMI Server",
+                Name = "USB WiFi HMI Server",
+                ImageIndex = (int)AppTree.Image.UsbWifiHmiServer,
+                Tag = new UsbWifiHmiServer()
+            },
+            new ListViewItem()
+            {
+                Text = "HMI Web Server Station",
+                Name = "HMI Web Server Station",
+                ImageIndex = (int)AppTree.Image.HmiServerStation,
+                Tag = new HmiServerStation()
+            },
+        };
+
+        private ListViewItem[] filesItems =
+        {
+            new ListViewItem()
+            {
+                Text = "Screen File",
+                Name = "NewScreen.html",
+                ImageIndex = (int)AppTree.Image.Html,
+                Tag = new FileInfo(Directory + "\\NewScreen.html")
+            },
+            new ListViewItem()
+            {
+                Text = "Style File",
+                Name = "NewStyle.css",
+                ImageIndex = (int)AppTree.Image.Css,
+                Tag = new FileInfo(Directory + "\\NewStyle.css")
+            },
+            new ListViewItem()
+            {
+                Text = "Script File",
+                Name = "NewScript.js",
+                ImageIndex = (int)AppTree.Image.Javascript,
+                Tag = new FileInfo(Directory + "\\NewScript.js")
+            },
+            new ListViewItem()
+            {
+                Text = "Data File",
+                Name = "NewData.json",
+                ImageIndex = (int)AppTree.Image.Json,
+                Tag = new FileInfo(Directory + "\\NewData.json")
+            },
+        };
+
+        private List<ListViewItem[]> listViewItems;
+
+        public AddItem()
+        {
+            InitializeComponent();
+            listViewItems = new List<ListViewItem[]>();
+            listViewItems.Add(dataAquisitionItems);
+            listViewItems.Add(dataServerItems);
+            listViewItems.Add(devicesItems);
+            listViewItems.Add(filesItems);
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            int idx = e.Node.Index;
+            listView.Clear();
+            listView.Items.AddRange(listViewItems[idx]);
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var list = sender as ListView;
+            if (list == null || list.SelectedItems.Count == 0) return;
+            var it = list.SelectedItems[0];
+            tbName.Text = it.Name;
+            ItemToAdd = it.Tag;
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void tbName_TextChanged(object sender, EventArgs e)
+        {
+            var tb = sender as TextBox;
+            if(ItemToAdd != null)
+            {
+                if(!(ItemToAdd is FileInfo))
+                {
+                    ItemToAdd.Name = tb.Text;
+                }
+                else
+                {
+                    ItemToAdd = new FileInfo(Directory + "\\" + tb.Text);
+                }
+            }
+        }
+    }
+
+}
