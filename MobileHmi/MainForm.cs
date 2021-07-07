9935486fc301836466ec9aa89971906866902c9e
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-07-07 09:47:31 -0300

    refact: name changed

--- MobileHmi/MainForm.cs
+++ MobileHmi/MainForm.cs
@@ -55,7 +55,7 @@ namespace MobileHmi
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            FileDialog.Save(AppData);
+            FileDialog.SaveAs(AppData);
         }
 
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
